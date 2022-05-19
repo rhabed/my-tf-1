8e2818241ed93817d38c3282219c2ee8a1ca894c
Robert Abed <rob.abed@vodafone.com.au>
2022-05-19 11:03:48 +1000

    update command with region

--- prod.tf
+++ prod.tf
@@ -13,6 +13,6 @@ resource "null_resource" "ec2_host_fleet" {
         number_of_host = "robert"
     }
     provisioner "local-exec" {
-        command = "aws ec2 allocate-hosts --instance-family \"a1\" --availability-zone \"ap-southeast-2a\" --auto-placement \"off\" --host-recovery \"on\" --quantity ${var.host_count} "
+        command = "aws ec2 allocate-hosts --instance-family \"a1\" --availability-zone \"ap-southeast-2a\" --auto-placement \"off\" --host-recovery \"on\" --quantity ${var.host_count} --region ${var.region}"
     }
 }
