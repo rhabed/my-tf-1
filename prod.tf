4036ee975280fb4d22f0cbfb72dc993d8ae5e7f0
Robert Abed <rob.abed@vodafone.com.au>
2022-05-19 11:09:56 +1000

    change trigger

--- prod.tf
+++ prod.tf
@@ -10,7 +10,7 @@
 
 resource "null_resource" "ec2_host_fleet" {
     triggers = {
-        number_of_host = "robert"
+        number_of_host = "my_robert"
     }
     provisioner "local-exec" {
         command = "aws ec2 allocate-hosts --instance-family \"a1\" --availability-zone \"ap-southeast-2a\" --auto-placement \"off\" --host-recovery \"on\" --quantity ${var.host_count} --region ${var.region}"
