44a6794549b5143dbdd3334e62c0e1dd85f47a79
Robert Abed <rob.abed@vodafone.com.au>
2022-05-19 12:03:01 +1000

    update with ;

--- prod.tf
+++ prod.tf
@@ -19,7 +19,7 @@ resource "null_resource" "ec2_host_fleet" {
     provisioner "local-exec" {
         interpreter = ["/bin/bash", "-c"]
         command = <<-EOT
-            aws ec2 describe-regions
+            aws ec2 describe-regions;
             aws ec2 allocate-hosts --instance-family \"a1\" --availability-zone ${random_shuffle.az.result[0]} --auto-placement \"off\" --host-recovery \"on\" --quantity ${var.host_count} --region ${var.region}
         EOT
     }
