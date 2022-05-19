e86b206090029617834b5c0fc317b2a9f72a07f1
Robert Abed <rob.abed@vodafone.com.au>
2022-05-19 11:24:13 +1000

    update - retrigger

--- prod.tf
+++ prod.tf
@@ -14,7 +14,7 @@ resource "random_shuffle" "az" {
 
 resource "null_resource" "ec2_host_fleet" {
     triggers = {
-        number_of_host = "my_robert"
+        number_of_host = "trigger"
     }
     provisioner "local-exec" {
         command = "aws ec2 allocate-hosts --instance-family \"a1\" --availability-zone ${random_shuffle.az.result} --auto-placement \"off\" --host-recovery \"on\" --quantity ${var.host_count} --region ${var.region}"
