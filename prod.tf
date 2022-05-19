83578432dd1481b7c88063e846c0303a3ff6baad
Robert Abed <rob.abed@vodafone.com.au>
2022-05-19 11:21:07 +1000

    update - add ramdon AZ

--- prod.tf
+++ prod.tf
@@ -7,12 +7,16 @@
 #   auto_placement    = "on"
 
 # }
+resource "random_shuffle" "az" {
+  input        = ["ap-southeast-2a", "ap-southeast-2b"]
+  result_count = 1
+}
 
 resource "null_resource" "ec2_host_fleet" {
     triggers = {
         number_of_host = "my_robert"
     }
     provisioner "local-exec" {
-        command = "aws ec2 allocate-hosts --instance-family \"a1\" --availability-zone \"ap-southeast-2a\" --auto-placement \"off\" --host-recovery \"on\" --quantity ${var.host_count} --region ${var.region}"
+        command = "aws ec2 allocate-hosts --instance-family \"a1\" --availability-zone ${random_shuffle.az.result} --auto-placement \"off\" --host-recovery \"on\" --quantity ${var.host_count} --region ${var.region}"
     }
 }
