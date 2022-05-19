280d4b58a790c3c69956f8553290d4acb5cb09f9
Robert Abed <rob.abed@vodafone.com.au>
2022-05-19 11:59:02 +1000

    update

--- prod.tf
+++ prod.tf
@@ -14,12 +14,12 @@ resource "random_shuffle" "az" {
 
 resource "null_resource" "ec2_host_fleet" {
     triggers = {
-        number_of_host = "trigger3"
+        number_of_host = "trigger4"
     }
     provisioner "local-exec" {
         interpreter = ["/bin/bash", "-c"]
         command = <<-EOT
-            echo \"robert\"
+            aws ec2 describe-regions
             aws ec2 allocate-hosts --instance-family \"a1\" --availability-zone ${random_shuffle.az.result[0]} --auto-placement \"off\" --host-recovery \"on\" --quantity ${var.host_count} --region ${var.region}
         EOT
     }
