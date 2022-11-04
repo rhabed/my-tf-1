8407082634e5db6635edda4289b740da320ad845
Robert Abed <rob.abed@vodafone.com.au>
2022-11-04 14:23:13 +1100

    revert

--- ec2_host/ec2-host.tf
+++ ec2_host/ec2-host.tf
@@ -1,15 +1,15 @@
 
 variable "az" {
-    default = ["ap-southeast-2a", "ap-southeast-2b"]
+    default = ["ap-southeast-2a", "ap-southeast-2c"]
 }
 
 variable "subnet_ids" {
-    default = ["subnet-fb2cb79f", "subnet-9175f7e7"]
+    default = ["subnet-fb2cb79f", "subnet-bfd877e6"]
 }
 
 resource "aws_ec2_host" "host" {
     count = 2
-    instance_type = "mac1.metal"
+    instance_family = "m4"
     host_recovery =  "off"
     auto_placement = "off"
     availability_zone = var.az[count.index % 2]
@@ -17,26 +17,25 @@ resource "aws_ec2_host" "host" {
 
 resource "aws_instance" "instance" {
     count = 2
-    ami = "ami-0a99fd5afc0ce4d09" 
-    #data.aws_ami.ubuntu.id
-    instance_type = "mac1.metal"
+    ami = data.aws_ami.ubuntu.id
+    instance_type = "m4.large"
     availability_zone = var.az[count.index % 2]
     subnet_id = var.subnet_ids[count.index % 2]
     host_id = aws_ec2_host.host[count.index].id
 }
 
-# data "aws_ami" "ubuntu" {
-#   most_recent = true
+data "aws_ami" "ubuntu" {
+  most_recent = true
 
-#   filter {
-#     name   = "name"
-#     values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-#   }
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
 
-#   filter {
-#     name   = "virtualization-type"
-#     values = ["hvm"]
-#   }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
 
-#   owners = ["099720109477"] # Canonical
-# }
+  owners = ["099720109477"] # Canonical
+}
